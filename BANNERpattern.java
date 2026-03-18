132e149e9296b12bd5feb6e49221d287da99e26c
Ranjith <ranjith13506@gmail.com>
2026-03-18 10:35:33 +0530

    Initial commit: add OOPSBannerApps.java

---
+++ BANNERpattern.java
@@ -0,0 +1,12 @@
+public class BannerPattern {
+    public static void main(String[] args) {
+        // Cleaner code using String.join(" ") to join parts of each line with spaces
+        System.out.println(String.join(" ", "*", "**", "***", "****"));
+        System.out.println(String.join(" ", "****", "***", "**", "*"));
+        System.out.println(String.join(" ", "*", "**", "***", "****"));
+        System.out.println(String.join(" ", "****", "***", "**", "*"));
+        System.out.println(String.join(" ", "*", "**", "***", "****"));
+        System.out.println(String.join(" ", "****", "***", "**", "*"));
+        System.out.println(String.join(" ", "*", "**", "***", "****"));
+    }
+}
