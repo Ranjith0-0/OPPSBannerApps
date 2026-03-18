ccf26382e487ee22439fc3f8c69e8fc8e746f4a5
Ranjith <ranjith13506@gmail.com>
2026-03-18 09:27:53 +0530

    Initial commit: add HelloWorld Java file

---
+++ UC2OOPSBannerApp.java
@@ -0,0 +1,21 @@
+public class UC2OOPSBannerApp {
+    public static void main(String[] args) {
+        // Each string represents one row of the banner
+        String b1 = " ***   " + "  ***   " + " *****  " + " *****  ";
+        String b2 = "*   *  " + " *   *  " + "*     * " + "*       ";
+        String b3 = "*   *  " + " *   *  " + "*     * " + "*       ";
+        String b4 = "*   *  " + " *   *  " + "*****   " + " *****  ";
+        String b5 = "*   *  " + " *   *  " + "*        " + "     *  ";
+        String b6 = "*   *  " + " *   *  " + "*        " + "     *  ";
+        String b7 = " ***   " + "  ***   " + "*        " + " *****  ";
+
+        // Print each row in order
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+        System.out.println(b5);
+        System.out.println(b6);
+        System.out.println(b7);
+    }
+}
